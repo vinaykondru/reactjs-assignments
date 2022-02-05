6cc8ccf424642ae7101fed3439b161bc8072f40e
vinay <vinaykumarkondru@gmail.com>
2022-02-05 20:30:22 +0530

    assign2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -4,13 +4,34 @@ import './App.css';
 
 
 function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+  const { employee, title } = props;
+
+  return (
+    <div className='child'>
+      <div className="image"><img src={employee.profileImg} alt="" srcset="" width="100px" /></div>
+      <div className='heading'><h1>{employee.name}</h1></div>
+
+      <div className='fnt'>
+        <p >Location</p>
+      </div>
+      <div className="fntstle"><p>{employee.location}</p>
+        <br></br></div>
+
+
+      <div className="fnt"> <p>Blood group</p></div>
+      <div className="fntstle"> <p>{employee.bloodGroup}</p>
+        <br /></div>
+
+
+      <div className="fnt"><p >Age</p></div>
+      <div className="fntstle"><p >{employee.age}</p></div>
+
+
+
+
+
+    </div>
+  )
 }
 
 
